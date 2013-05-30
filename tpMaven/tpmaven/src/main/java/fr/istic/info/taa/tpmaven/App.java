c9f4b85b0192ef78f085c40690267fb321210ef5
29006342 <29006342@i202m03.istic.univ-rennes1.fr>
2013-05-30 19:12:47 +0200

    Complement du tp Maven

--- tpMaven/tpmaven/src/main/java/fr/istic/info/taa/tpmaven/App.java
+++ tpMaven/tpmaven/src/main/java/fr/istic/info/taa/tpmaven/App.java
@@ -1,6 +1,26 @@
 package fr.istic.info.taa.tpmaven;
 
-public class App 
-{
+public class App {
+	public String coco;
+	public String getCoco() {
+		return coco;
+	}
+
+	public int getNono() {
+		return nono;
+	}
+
+	public void setNono(int nono) {
+		this.nono = nono;
+	}
+
+	public void setCoco(String coco) {
+		this.coco = coco;
+	}
+
+	public int nono;
+   public static void main(String[] args) {
+	
+   }
    
 }
